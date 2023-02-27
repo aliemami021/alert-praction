dcddd3e9e3f836a64b6b3c67306e05c1e49914ef
ali <you@example.comcl>
2023-02-27 15:43:06 +0330

    alert-praction-editted

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let slogan = alert("Hey Teacher Leave Those Kids Alone and Enter 5 score!");
+
+let teacherScores1 = prompt("enter the first number")
+let teacherScores2 = prompt("enter the secound number")
+let teacherScores3 = prompt("enter the third number")
+let teacherScores4 = prompt("enter the fourth number")
+let teacherScores5 = prompt("enter the fifth number")
+
+let avg = (Number(teacherScores1) + Number(teacherScores2) + Number(teacherScores3) + Number(teacherScores4) + Number(teacherScores5)) / 5;
+alert("the avarege is:" + " " + avg);
